e2ffa709c35d1dd1bafe89c83b63c96429a383b3
jessi <jessi@jerome.dhcp.wustl.edu>
2023-10-04 14:16:31 -0500

    cat!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,30 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledSquare(1.0, 1.0, 1.0);
+
+		StdDraw.setPenColor(Color.GRAY);
+		StdDraw.filledCircle(.5, .5, .25);
+
+		double[] x = {0.25, 0.3, 0.45};
+		double[] y = {0.52, 0.87, 0.73};
+		StdDraw.filledPolygon(x, y);
 		
+		double[] a = { 0.45, 0.7, 0.75};
+		double[] b = { 0.52, 0.87, 0.53};
+		StdDraw.filledPolygon(a,b);
+		
+		StdDraw.setPenColor(Color.WHITE);
+		// left whiskers
+		StdDraw.line(0.1, 0.6, 0.3, 0.55);
+		StdDraw.line(0.1, 0.50, 0.3, 0.50);
+		
+		// right whiskers
+		StdDraw.line(0.7, 0.55, 0.9, 0.6);
+		StdDraw.line(0.7, 0.50, 0.9, 0.50);
 	}
+
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,46 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+
+		Color color = new Color(red,green, blue);
+		StdDraw.setPenColor(color);
+
+		boolean isFilled = in.nextBoolean();
+
+		//		double x = in.nextDouble();
+		//		double y = in.nextDouble();
+		//		double halfWidth = in.nextDouble();
+		//		double halfHeight = in.nextDouble();
+
+		//String shapeType, int redComponent, int greenComponent, int blueComponent, boolean isFilled
+
+
+		if(shape.equals("triangle")) 
+		{			
+			double[] x = new double[3];
+			double[] y = new double[3];
+
+			for(int i = 0; i < x.length; i++) {
+				x[i] = in.nextDouble();
+				y[i] = in.nextDouble();
+			}
+			StdDraw.filledPolygon(x,y);
+		}
+
+
+
+		//		if(isFilled)
+		//		{
+		//			StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+		//		}
+		//		else {
+		//			StdDraw.rectangle(x,y,halfWidth,halfHeight);
+		//		}
 	}
 }
